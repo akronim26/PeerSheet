9fa0743cc3355b290e0b09add383b27d0a9833a2
Abhivansh <31abhivanshj@gmail.com>
2026-02-15 17:00:26 +0530

    refactor: refactor identity fetching

--- utils/helpers.go
+++ utils/helpers.go
@@ -9,49 +9,27 @@ import (
 	"github.com/libp2p/go-libp2p/core/peer"
 )
 
-var (
-	PublicKey crypto.PubKey
-	PeerID    peer.ID
-)
-
-func initKeysFromEnv() error {
+func LoadIdentityFromEnv() (crypto.PrivKey, peer.ID, error) {
 	privKeyGen := os.Getenv("PRIVATE_KEY_GEN")
 	if privKeyGen == "" {
-		return errors.New("PRIVATE_KEY_GEN environment variable is not set")
+		return nil, "", errors.New("PRIVATE_KEY_GEN environment variable is not set")
 	}
 
 	data, err := base64.StdEncoding.DecodeString(privKeyGen)
 	if err != nil {
-		return errors.New("failed to decode private key generator string: " + err.Error())
+		return nil, "", errors.New("failed to decode private key generator string: " + err.Error())
 	}
 
 	privKey, err := crypto.UnmarshalPrivateKey(data)
 	if err != nil {
-		return errors.New("failed to generate private key: " + err.Error())
+		return nil, "", errors.New("failed to generate private key: " + err.Error())
 	}
 
-	PublicKey = privKey.GetPublic()
+	pubKey := privKey.GetPublic()
 
-	peerID, err := peer.IDFromPublicKey(PublicKey)
+	peerID, err := peer.IDFromPublicKey(pubKey)
 	if err != nil {
-		return errors.New("failed to generate peer id: " + err.Error())
+		return nil, "", errors.New("failed to generate peer id: " + err.Error())
 	}
-	PeerID = peerID
-	return nil
-}
-
-func GetPublicKey() (crypto.PubKey, error) {
-	err := initKeysFromEnv()
-	if err != nil {
-		return nil, err
-	}
-	return PublicKey, nil
-}
-
-func GetPeerID() (peer.ID, error) {
-	err := initKeysFromEnv() 
-	if err != nil {
-		return "", err
-	}
-	return PeerID, nil
+	return privKey, peerID, nil
 }
